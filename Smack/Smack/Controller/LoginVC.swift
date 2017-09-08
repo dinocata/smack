bc8fd6e09c9c3c793c6567fd178b56bc6d037f2f
Dino <catalinac@uhp-digital.com>
2017-09-08 15:18:15 +0200

    Login Screen

---
+++ Smack/Smack/Controller/LoginVC.swift
@@ -0,0 +1,23 @@
+//
+//  LoginVC.swift
+//  Smack
+//
+//  Created by UHP Mac 3 on 08/09/2017.
+//  Copyright © 2017 UHP Mac 3. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func closePressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+
+}
---
+++ Smack/Smack/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Smack
+//
+//  Created by UHP Mac 3 on 08/09/2017.
+//  Copyright © 2017 UHP Mac 3. All rights reserved.
+//
+
+import Foundation
+
+// Segues
+let TO_LOGIN = "toLogin"
