4f5b3622061954efbcdb12a23334d120f865a524
Dino <catalinac@uhp-digital.com>
2017-09-14 13:54:55 +0200

    Choosing avatar

---
+++ Smack/Smack/View/CircleImage.swift
@@ -0,0 +1,28 @@
+//
+//  CircleImage.swift
+//  Smack
+//
+//  Created by UHP Mac 3 on 14/09/2017.
+//  Copyright © 2017 UHP Mac 3. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class CircleImage: UIImageView {
+
+    override func awakeFromNib() {
+        setupView()
+    }
+    
+    func setupView() {
+        self.layer.cornerRadius = self.frame.width / 2
+        self.clipsToBounds = true
+    }
+    
+    override func prepareForInterfaceBuilder() {
+        super.prepareForInterfaceBuilder()
+        setupView()
+    }
+
+}
