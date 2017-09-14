8f7a7d6a78e8527eeed1d5d122dc9ea083de5baf
Dino <catalinac@uhp-digital.com>
2017-09-14 11:55:49 +0200

    Added collection view for avatar picker

---
+++ Smack/Smack/View/AvatarCell.swift
@@ -0,0 +1,25 @@
+//
+//  AvatarCell.swift
+//  Smack
+//
+//  Created by UHP Mac 3 on 14/09/2017.
+//  Copyright © 2017 UHP Mac 3. All rights reserved.
+//
+
+import UIKit
+
+class AvatarCell: UICollectionViewCell {
+    
+    @IBOutlet weak var avatarImg: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        setupView()
+    }
+    
+    func setupView() {
+        self.layer.backgroundColor = UIColor.lightGray.cgColor
+        self.layer.cornerRadius = 10
+        self.clipsToBounds = true
+    }
+}
