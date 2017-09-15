f291573ce7e17122cea1c71a304ae6c7998d8371
Dino <catalinac@uhp-digital.com>
2017-09-15 13:31:55 +0200

    Showing messages in channel

---
+++ Smack/Smack/View/MessageCell.swift
@@ -0,0 +1,32 @@
+//
+//  MessageCell.swift
+//  Smack
+//
+//  Created by UHP Mac 3 on 15/09/2017.
+//  Copyright © 2017 UHP Mac 3. All rights reserved.
+//
+
+import UIKit
+
+class MessageCell: UITableViewCell {
+    
+    // Outlets
+    @IBOutlet weak var userImg: CircleImage!
+    @IBOutlet weak var userNameLbl: UILabel!
+    @IBOutlet weak var timestampLbl: UILabel!
+    @IBOutlet weak var messageBody: UILabel!
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    func configureCell(message: Message) {
+        messageBody.text = message.message
+        userNameLbl.text = message.username
+        userImg.image = UIImage(named: message.userAvatar)
+        userImg.backgroundColor = UserDataService.instance.returnUIColor(components: message.userAvatarColor)
+    }
+
+}
