5466eb25eb4827ef222426a8fb7d511f6ec45897
Dino <catalinac@uhp-digital.com>
2017-09-15 11:14:59 +0200

    Opening channels

--- Smack/Smack/Services/UserDataService.swift
+++ Smack/Smack/Services/UserDataService.swift
@@ -66,6 +66,7 @@ class UserDataService {
         AuthService.instance.isLoggedIn = false
         AuthService.instance.userEmail = ""
         AuthService.instance.authToken = ""
+        MessageService.instance.clearChannels()
     }
     
     
