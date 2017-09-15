bf0f2315ef12bbb7e74b17ea43b6502f99398491
Dino <catalinac@uhp-digital.com>
2017-09-15 11:51:45 +0200

    Getting messages

---
+++ Smack/Smack/Model/Message.swift
@@ -0,0 +1,20 @@
+//
+//  Message.swift
+//  Smack
+//
+//  Created by UHP Mac 3 on 15/09/2017.
+//  Copyright © 2017 UHP Mac 3. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    
+    public private(set) var message: String!
+    public private(set) var username: String!
+    public private(set) var channelId: String!
+    public private(set) var userAvatar: String!
+    public private(set) var userAvatarColor: String!
+    public private(set) var id: String!
+    public private(set) var timestamp: String!
+}
